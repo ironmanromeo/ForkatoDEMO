9b496524be3cac58d23a80f105d02508e47b1a9b
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-05 16:36:38 +0000

    Update 3 files

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanillfffddddfddfda!</h1>
 <div>
-  We use the same pipppo as Parcel to bundle this sandbox, you can find more
+  We use the same BARBARA 2 as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>`;
-
-console.log("antani aaa");
