8acaeeb12d03c9afbe9cf1617557b563f5da3348
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-06 10:49:10 +0000

    aggiunto mmmm

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanillfffddddfddfda!</h1>
 <div>
-  We use the same BARBARA 2 as Parcel to bundle this sandbox, you can find more
+  We use the same BARBARA 2 ammmms Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>`;
